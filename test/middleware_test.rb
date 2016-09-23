94789525ad1a195749def167c2460a270aa590a0
Adam Hess <adamhess1991@gmail.com>
2016-09-23 09:56:41 -0700

    make log tests more specific

--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -15,9 +15,15 @@ class BustedTracerTest < Minitest::Test
   end
 
   def test_returns_result_exactly
+    log = MockLogger.new
     app = proc {|env| [200, {'Content-Type' => 'text/plain'}, ['OK']]}
     middleware = BustedTracer.new(app)
-    assert_equal [200, {'Content-Type' => 'text/plain'}, ['OK']], middleware.call({})
+
+    # This stub is not strictly needed, but it prevents logging
+    # During a test run.
+    middleware.stub :logger, log do
+      assert_equal [200, {'Content-Type' => 'text/plain'}, ['OK']], middleware.call({})
+    end
   end
 
   def test_it_logs_cache_invalidations
@@ -28,7 +34,7 @@ class BustedTracerTest < Minitest::Test
       middleware.call({})
     end
 
-    assert_match(/methods=0 constants=0/, log.messages.last)
+    assert_equal "[Cache Invalidations] methods=0 constants=0", log.messages.last
   end
 
   def test_it_logs_method_cache_correctly
@@ -42,7 +48,7 @@ class BustedTracerTest < Minitest::Test
       middleware.call({})
     end
 
-    assert_match(/methods=1 constants=0/, log.messages.last)
+    assert_equal "[Cache Invalidations] methods=1 constants=0", log.messages.last
   end
 
   def test_it_logs_constant_cache_correctly
@@ -56,6 +62,6 @@ class BustedTracerTest < Minitest::Test
       middleware.call({})
     end
 
-    assert_match(/methods=0 constants=1/, log.messages.last)
+    assert_equal "[Cache Invalidations] methods=0 constants=1", log.messages.last
   end
 end
